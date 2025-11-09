390f1233d921e902fb81b991d8563e2476f40849
vek03 <vektromboni@gmail.com>
2025-11-09 17:59:52 -0300

    :tada: Estrutura

---
+++ spring/src/main/java/com/vekrest/veksecurity/VeksecurityApplication.java
@@ -0,0 +1,13 @@
+package com.vekrest.veksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeksecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeksecurityApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/vekrest/veksecurity/VeksecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekrest.veksecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeksecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
