a641ed0ad3a287904d46ef0e1fc37efd5270ea0f
vek03 <vektromboni@gmail.com>
2025-12-16 20:08:35 -0300

    fix: url

--- spring/src/main/java/com/vekrest/veksecurity/veksecurity/controller/UserController.java
+++ spring/src/main/java/com/vekrest/veksecurity/veksecurity/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/vekrest/veksecurity/v1")
 public class UserController {
     private final UserRepository repository;
 
